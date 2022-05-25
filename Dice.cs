c2a60dfbeb77bbb6922d92c4da93f147acf3c33a
darya3d <106013073+darya3d@users.noreply.github.com>
2022-05-25 20:18:44 +0400

    Add files via upload

---
+++ Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Игра началась...нажимайте пробел для броска");
+            Random r = new Random();
+            while (true)
+            {
+                Console.WriteLine("Ваши выпавшие кубики ");
+                int kub = r.Next(1, 7);
+                int kob = r.Next(1, 7);
+
+                Console.WriteLine($"{kub} {kob}");
+                Console.ReadKey();
+            }
+        
+        }
+    }
+}
